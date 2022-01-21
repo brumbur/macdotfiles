ceaf9d2c03a96293efd7d920e51123fd7a216b8e
brumbur <4alexander@gmail.com>
2022-01-20 23:17:33 -0500

    update scripts

--- install.sh
+++ install.sh
@@ -4,17 +4,16 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo "source ~/.zshrc-custom" >> ~/.zshrc
-ln -s $SCRIPT_DIR/.zshrc-custom ~/.zshrc-custom
+echo "source ~/.zshrc-local" >> ~/.zshrc
+ln -s $SCRIPT_DIR/.zshrc-local ~/.zshrc-local
 
 # brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-test -f $SCRIPT_DIR/Brewfile && brew bundle --file=~$SCRIPT_DIR/Brewfile || ./brew-packages.sh
-
+test -f $SCRIPT_DIR/Brewfile && brew bundle --file=~$SCRIPT_DIR/Brewfile || source ./brew-packages.sh
 
 # options
 
-# antigen -> all plugins already configured in .zshrc-custom
+# antigen -> all plugins already configured in .zshrc-local
 curl -L git.io/antigen > antigen.zsh
 
 # colorls (to show file icons, etc.) or just use exa
@@ -25,4 +24,3 @@ gem install colorls
 # git config
 # pip config
 # aws config
-
