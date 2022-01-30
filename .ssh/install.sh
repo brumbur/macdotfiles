4b20fa4c1efcc39d6e53f8087c1bdecc9a97ef15
brumbur <4alexander@gmail.com>
2022-01-30 16:17:31 -0500

    updating scripts

--- .ssh/install.sh
+++ .ssh/install.sh
@@ -3,7 +3,7 @@ SCRIPT_DIR=${0:A:h}
 set -o magicequalsubst && USER_HOME=~
 
 brew install openssh
-$ export PATH=$(brew --prefix openssh)/bin:$PATH
+export PATH=$(brew --prefix openssh)/bin:$PATH
 
 # ssh config docs: https://man.openbsd.org/ssh_config.5
 # check also /etc/ssh/ssh_config
