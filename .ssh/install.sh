1914d070b4912bcd96fb3dbe52dec8576f0a951d
brumbur <4alexander@gmail.com>
2022-01-29 19:57:09 -0500

    updating scripts

--- .ssh/install.sh
+++ .ssh/install.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env zsh
+SCRIPT_DIR=${0:A:h}
+set -o magicequalsubst && USER_HOME=~
+
+brew install openssh
+$ export PATH=$(brew --prefix openssh)/bin:$PATH
+
 # ssh config docs: https://man.openbsd.org/ssh_config.5
 # check also /etc/ssh/ssh_config
 
-# check and create config as needed
-[[ ! -f ~/.ssh/config ]]; then
-	[[ ! -d ~/.ssh ]] && mkdir -m 600 ~/.ssh
-
-
-fi
-
+# just copy hte template file to help create the actual one
+[[ ! -d ~/.ssh ]] && mkdir -m 600 ~/.ssh && cp $SCRIPT_DIR/config.tmpl ~/.ssh
