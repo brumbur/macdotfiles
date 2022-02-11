b6696eb577ab4c579f9f37ee2b3fdca26a3c9dcf
brumbur <4alexander@gmail.com>
2022-02-11 00:01:32 -0500

    fixing p10 script

--- install.sh
+++ install.sh
@@ -28,14 +28,14 @@ if ! grep -q "export PATH_PREFIX" "$USER_HOME/.zprofile"; then
 	printf "%s\n" >> ~/.zprofile
 	printf 'eval $($PATH_PREFIX/bin/brew shellenv)' >> ~/.zprofile
 fi
-# [[ -f $SCRIPT_DIR/homebrew/Brewfile ]] && $PATH_PREFIX/bin/brew bundle --file=$SCRIPT_DIR/homebrew/Brewfile || \
-# 	"Note: Could not find Bundle file, please install yourself the packages you want"
+[[ -f $SCRIPT_DIR/homebrew/Brewfile ]] && $PATH_PREFIX/bin/brew bundle --file=$SCRIPT_DIR/homebrew/Brewfile || \
+	"Note: Could not find Bundle file, please install yourself the packages you want"
 
 # Python3
-# pip3 install --user pipenv &>/dev/null
-# pip3 install virtualenv &>/dev/null
-# pip3 install virtualenvwrapper &>/dev/null
-# python3 -m pip install --upgrade pip &>/dev/null
+pip3 install --user pipenv &>/dev/null
+pip3 install virtualenv &>/dev/null
+pip3 install virtualenvwrapper &>/dev/null
+python3 -m pip install --upgrade pip &>/dev/null
 
 # Docker Desktop  (macos only)
 DOCKER_URL="https://desktop.docker.com/mac/main/amd64/Docker.dmg"
@@ -59,12 +59,6 @@ echo " -- installing options from $SCRIPT_DIR"
 # antigen
 [[ ! -f "$USER_HOME/antigen.zsh" ]] && curl -L git.io/antigen > antigen.zsh &>/dev/null
 
-# all antigen plugins already configured in .zshrc-local
-if [[ -f "$SCRIPT_DIR/shell/.zshrc-local" ]] && ! grep -q "zshrc-local" "$USER_HOME/.zshrc"; then
-	printf "%s\n"  >> ~/.zshrc
-	printf "source $SCRIPT_DIR/shell/.zshrc-local" >> ~/.zshrc
-fi
-
 # source custom vars and paths
 if [[ -f "$SCRIPT_DIR/shell/.zprofile-local" ]]  && ! grep -q "zprofile-local" $USER_HOME/.zprofile; then
 	printf "%s\n"  >> ~/.zprofile
@@ -72,28 +66,33 @@ if [[ -f "$SCRIPT_DIR/shell/.zprofile-local" ]]  && ! grep -q "zprofile-local" $
 fi
 
 # source p10k
-if [[ -f "$SCRIPT_DIR/shell/.p10k-local.zsh" ]]  && ! grep -q "source ~/.p10k.zsh" $USER_HOME/.zshrc; then
+if [[ -f "$SCRIPT_DIR/shell/.p10k.zsh" ]]  && ! grep -q "source ~/.p10k.zsh" $USER_HOME/.zshrc; then
 	echo " -- configuring p10k prmpt..."
-	{ printf "%s" <<-EOF
-			# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-			# Initialization code that may require console input (password prompts, [y/n]
-			# confirmations, etc.) must go above this block; everything else may go below.
-			if [[ -r \"${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh\" ]]; then
-			  source \"${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh\"
-			fi
-		EOF
-		cat ~/.zshrc 
-	} > ~/.zshrc.new
+cat <<-EOF > ~/.zshrc.new
+	# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+	# Initialization code that may require console input (password prompts, [y/n]
+	# confirmations, etc.) must go above this block; everything else may go below.
+	if [[ -r \"${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh\" ]]; then
+	  source \"${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh\"
+	fi
+	
+EOF
+	cat ~/.zshrc >> ~/.zshrc.new
 
 	# { printf "${p10k}"; cat ~/.zshrc } > ~/.zshrc.new
 	cp ~/.zshrc ~/.zshrc.bak
 	mv ~/.zshrc.new ~/.zshrc
 
-	printf "%s\n" >> ~/.zshrc
-	printf "# To customize prompt, run \`p10k configure\` or edit ~/.p10k.zsh." >> ~/.zshrc
+	printf "\n%s\n" "# To customize prompt, run \`p10k configure\` or edit ~/.p10k.zsh." >> ~/.zshrc
 	printf "[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh" >> ~/.zshrc
 
-	cp "$SCRIPT_DIR/shell/.p10k-local.zsh" ~/.p10k.zsh
+	cp "$SCRIPT_DIR/shell/.p10k.zsh" ~/.p10k.zsh
+fi
+
+# all antigen plugins already configured in .zshrc-local
+if [[ -f "$SCRIPT_DIR/shell/.zshrc-local" ]] && ! grep -q "zshrc-local" "$USER_HOME/.zshrc"; then
+	printf "%s\n"  >> ~/.zshrc
+	printf "source $SCRIPT_DIR/shell/.zshrc-local" >> ~/.zshrc
 fi
 
 # hyper
